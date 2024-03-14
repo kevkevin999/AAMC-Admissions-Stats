470db59479c6b89a58e46730ba07747c58b03c50
kjx2 <94778542+kevkevin999@users.noreply.github.com>
2024-03-14 12:31:59 -0700

    find "content-container" box

--- parseData.py
+++ parseData.py
@@ -1,22 +1,27 @@
 import requests
 from bs4 import BeautifulSoup
 import selenium
+import time
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 # Inspired from here: https://realpython.com/beautiful-soup-web-scraper-python/
 URL = "https://mec.aamc.org/msar-ui/#/landing"
 
 driver = webdriver.Chrome()
 
+driver.get(URL)
+# html = driver.page_source
+time.sleep(2)
+# print(html)
 
-page = driver.get(URL)
-print(page)
 
 # page = requests.get(URL)
-soup = BeautifulSoup(page.content, "html.parser")
-results = soup.find(class_="content-container")
+# soup = BeautifulSoup(page.content, "html.parser")
+results = driver.find_elements(by=By.CLASS_NAME, value="content-container")
 print(results)
 
 # cards = soup.find_all("mat-card", class_="mat-mdc-card mdc-card ng-star-inserted")
 # print(cards)
 
+driver.quit()
